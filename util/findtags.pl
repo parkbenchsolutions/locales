c5e0036548ec6509b16459fe2fc5dadc7b4d59b3
Ken Burcham <kburcham@parkbenchsolutions.com>
2022-11-21 14:41:13 -0800

    tags

--- util/findtags.pl
+++ util/findtags.pl
@@ -3,7 +3,19 @@ use warnings;
 use strict;
 
 # my $topdirname = '/Users/kenburcham/code/odinweb/src/components/groups/service-settings/*';
-my $topdirname = '/Users/kenburcham/code/odinweb/src/components/bulk/bulk-enterprise-clone';
+# my $topdirname = '/Users/kenburcham/code/odinweb/src/components/bulk/bulk-enterprise-clone';
+# my $topdirname = '/Users/kenburcham/code/odinweb/src/components/bulk/*';
+# my $topdirname = '/Users/kenburcham/code/odinweb/src/components/audits/*';
+my $topdirname = '/Users/kenburcham/code/odinweb/src/components/system/*';
+my $topdirname = '/Users/kenburcham/code/odinweb/src/components/autoAttendant/*';
+my $topdirname = '/Users/kenburcham/code/odinweb/src/components/branding/*';
+my $topdirname = '/Users/kenburcham/code/odinweb/src/components/call-processing-policy/*';
+my $topdirname = '/Users/kenburcham/code/odinweb/src/components/departments/*';
+my $topdirname = '/Users/kenburcham/code/odinweb/src/components/events/*';
+my $topdirname = '/Users/kenburcham/code/odinweb/src/components/exports/*';
+my $topdirname = '/Users/kenburcham/code/odinweb/src/components/service-provider/*';
+my $topdirname = '/Users/kenburcham/code/odinweb/src/components/users/*';
+# my $topdirname = '/Users/kenburcham/code/odinweb/src/components/*';
 
 # opendir(DIR, $dirname) or die $!;
 my %errors = ();
@@ -57,34 +69,43 @@ sub getTags {
       #    $common{$2} = $2;
       # }
       #
-      # captures the label in the columns-like arrays of objects
-      while (/{?\s*key:\s?'(.*)',\s?\n?\s*label:\s?("|'|{`)(.*?)("|'|`})/g) {
-         $group{$3} = $3;
-      }
 
-      # captures the label in the UiTabs (also in specific namespace like 'group'
+      ####################
+
+
+      # # captures the label in the columns-like arrays of objects
+      # while (/{?\s*key:\s?'(.*)',\s?\n?\s*label:\s?("|'|{`)(.*?)("|'|`})/g) {
+      #    $group{$3} = $3;
+      # }
+      #
+      # # captures the label in the UiTabs (also in specific namespace like 'group'
       # while (/<div\s*label\s*=\s*{t\(('|")(.*)('|")\)}>/g) {
       #    $group{$2} = $2;
       # }
-
-      # <p>{t(' paragraphs that have translate
+      #
+      # # <p>{t(' paragraphs that have translate
       # while (/<p>\n?.*\{t\(("|'|{`)(.*?)("|'|`}).*\n?<\/p>/g) {
       #    $group{$2} = $2;
       # }
-
-      #naked wrapped strings
+      #
+      # #naked wrapped strings
       # while (/{t\(("|'|{`)(.*?)("|'|`})\)}/g) {
       #    $group{$2} = $2;
       # }
       #
 
       #other things to do
-      # {t('Enter Password')}
-      # <label>{t('No Pending Task available!')}</label>
+
       # &nbsp; {t('Leave Blank')}
       # <b>{t('Please enter number or ranges on separate line')}</b>
       # return Promise.resolve(t('Phone is added'))
       # <span> {t('Archive')}</span>
+
+      ## done
+      # <Breadcrumb.Item>{t('Create Auto Attendant')}</Breadcrumb.Item>
+      # while (/<Breadcrumb\.Item>{t\('(.*?)'/g) {
+      #    $common{$1} = $1;
+      # }
 
 
 
